dbabff8cee99e194b7c6b24f9f3a3feaeb91c07e
Kiril Klein <kikl@di.ku.dk>
2024-10-17 16:49:22 +0200

    First working version of aipw att

--- CausalEstimate/estimators/functional/aipw.py
+++ CausalEstimate/estimators/functional/aipw.py
@@ -7,9 +7,11 @@ ATE:
         "An introduction to the augmented inverse propensity weighted estimator." 
         Political analysis 18.1 (2010): 36-56.
         note: This also provides a variance estimator for the AIPW estimator.
+
+
 """
 
-from CausalEstimate.estimators.functional.ipw import compute_ipw_ate
+from CausalEstimate.estimators.functional.ipw import compute_ipw_ate, compute_ipw_att
 
 
 def compute_aipw_ate(A, Y, ps, Y0_hat, Y1_hat):
@@ -19,6 +21,58 @@ def compute_aipw_ate(A, Y, ps, Y0_hat, Y1_hat):
     Y0_hat: P[Y|A=0], Y1_hat: P[Y|A=1]
     """
     ate_ipw = compute_ipw_ate(A, Y, ps)
-    adjustment_factor = (A - ps) / (ps * (1 - ps))
+    adjustment_factor = compute_adjustment_factor(A, ps)
     ate = ate_ipw - adjustment_factor * ((1 - ps) * Y1_hat + ps * Y0_hat)
     return ate.mean()
+
+
+# def compute_aipw_att(A, Y, ps, Y0_hat, Y1_hat):
+#     """
+#     Augmented Inverse Probability of Treatment Weighting (AIPW) for ATT.
+#     A: treatment assignment, Y: outcome, ps: propensity score
+#     Y0_hat: P[Y|A=0], Y1_hat: P[Y|A=1]
+#     note: This is currently a guess, since no reference is found.
+#     Based on how the formula for IPW changes, we can infer that the formula for AIPW should change.
+#     !! Need to check this !!
+#     """
+#     att_ipw = compute_ipw_att(A, Y, ps)
+#     adjustment_factor = compute_adjustment_factor(A, ps)
+#     att = att_ipw - adjustment_factor * (Y1_hat + ps / (1 - ps) * Y0_hat)
+#     return att.mean()
+def compute_stabilized_att_weights(A, ps):
+    """
+    Compute the stabilized weights for the ATT estimator.
+    """
+    h = ps / (1 - ps)
+    return A + (1 - A) * h
+
+def compute_aipw_att(A, Y, ps, Y0_hat, Y1_hat):
+    """
+    Augmented Inverse Probability Weighting (AIPW) for ATT.
+    A: treatment assignment (binary), Y: outcome, ps: propensity score
+    Y0_hat: predicted outcome under control, Y1_hat: predicted outcome under treatment
+    """
+    # Compute the stabilized weights for ATT
+    W = compute_stabilized_att_weights(A, ps)
+    
+    # Numerator for the IPW ATT estimator
+    ipw_numer = (W * A * Y).sum() - (W * (1 - A) * Y).sum()
+    # Denominator for the IPW ATT estimator
+    ipw_denom = (W * A).sum()
+    # IPW ATT estimate
+    ipw_att = ipw_numer / ipw_denom
+    
+    # Augmentation term
+    augmentation = ((W * (1 - A) * (Y0_hat - Y1_hat)).sum()) / (W * A).sum()
+    
+    # Predicted means for treated units
+    mu1_hat = Y1_hat[A == 1].mean()
+    mu0_hat = Y0_hat[A == 1].mean()
+    
+    # Compute the AIPW ATT
+    att = ipw_att + augmentation + (mu1_hat - mu0_hat)
+    
+    return att
+
+def compute_adjustment_factor(A, ps):
+    return (A - ps) / (ps * (1 - ps))
