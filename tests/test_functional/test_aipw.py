d543caeebc7ae8778f7626f0343cb665c8ac0662
Kiril Klein <kikl@di.ku.dk>
2024-10-17 17:45:29 +0200

    Further refactoring of functional tests

--- tests/test_functional/test_aipw.py
+++ tests/test_functional/test_aipw.py
@@ -2,16 +2,11 @@ import unittest
 import numpy as np
 from CausalEstimate.estimators.functional.aipw import compute_aipw_ate, compute_aipw_att
 
-from tests.test_functional.base import TestEffectBase
+from tests.helpers.setup import TestEffectBase
 
 
 class TestComputeAIPWATE(TestEffectBase):
-    def test_aipw_ate_computation(self):
-        ate_aipw = compute_aipw_ate(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(ate_aipw, float)
-        # Check if the AIPW estimate is close to the true ATE
-        self.assertAlmostEqual(ate_aipw, self.true_ate, delta=0.1)
-
+    """Basic tests for AIPW estimators"""
     def test_invalid_input_shapes(self):
         # Test for mismatched input shapes
         A = np.array([1, 0, 1])
@@ -24,58 +19,34 @@ class TestComputeAIPWATE(TestEffectBase):
         with self.assertRaises(ValueError):
             compute_aipw_ate(A, Y, ps, Y0_hat, Y1_hat)
 
-    def test_edge_case_ps_close_to_zero_or_one(self):
-        # Test with ps very close to 0 or 1
-        ps_edge = self.ps.copy()
-        ps_edge[ps_edge < 0.01] = 0.01
-        ps_edge[ps_edge > 0.99] = 0.99
+class TestAIPW_ATE_base(TestEffectBase):
+    def test_compute_aipw_ate(self):
+        ate_aipw = compute_aipw_ate(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
+        self.assertAlmostEqual(ate_aipw, self.true_ate, delta=0.1)
 
-        # Compute the AIPW estimate with the edge case propensity scores
-        ate_aipw = compute_aipw_ate(self.A, self.Y, ps_edge, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(ate_aipw, float)
-        # Check if the estimate is still close to the true ATE
-        self.assertAlmostEqual(ate_aipw, self.true_ate, delta=0.15)
-
-class TestAIPW_ATE_ps_misspecified(TestEffectBase):
+class TestAIPW_ATE_ps_misspecified(TestAIPW_ATE_base):
     alpha = [0.1, 0.2, -0.3, 1]
-    def test_compute_aipw_ate(self):
-        ate_aipw = compute_aipw_ate(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(ate_aipw, float)
-        # Check if the estimate is still close to the true ATE
-        self.assertAlmostEqual(ate_aipw, self.true_ate, delta=0.1)
 
-class TestAIPW_ATE_outcome_model_misspecified(TestEffectBase):
+class TestAIPW_ATE_outcome_model_misspecified(TestAIPW_ATE_base):
     beta = [0.5, 0.8, -0.6, 0.3, 5]
-    def test_compute_aipw_ate(self):
-        ate_aipw = compute_aipw_ate(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(ate_aipw, float)
-        # Check if the estimate is still close to the true ATE
-        self.assertAlmostEqual(ate_aipw, self.true_ate, delta=0.1)
 
-class TestComputeAIPWATT(TestEffectBase):
-    def test_aipw_att_computation(self):
-        # Test the computation of AIPW ATT
+class TestAIPW_ATT_base(TestEffectBase):
+    def test_compute_aipw_att(self):
         att_aipw = compute_aipw_att(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(att_aipw, float)
-        # Check if the AIPW estimate is close to the true ATT
         self.assertAlmostEqual(att_aipw, self.true_att, delta=0.1)
 
-class TestComputeAIPWATT_outcome_model_misspecified(TestEffectBase):
+class TestAIPW_ATT_outcome_model_misspecified(TestAIPW_ATT_base):
+    beta = [0.5, 0.8, -0.6, 0.3, 5]
+
+class TestAIPW_ATT_ps_misspecified(TestAIPW_ATT_base):
+    alpha = [0.1, 0.2, -0.3, 1]
+
+class TestAIPW_ATT_PS_misspecified_and_OutcomeModel_misspecified(TestAIPW_ATT_base):
+    alpha = [0.1, 0.2, -0.3, 5]
     beta = [0.5, 0.8, -0.6, 0.3, 5]
     def test_compute_aipw_att(self):
         att_aipw = compute_aipw_att(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(att_aipw, float)
-        # Check if the estimate is still close to the true ATT
-        self.assertAlmostEqual(att_aipw, self.true_att, delta=0.1)
-
-class TestComputeAIPWATT_ps_misspecified(TestEffectBase):
-    alpha = [0.1, 0.2, -0.3, 1]
-    def test_compute_aipw_att(self):
-        att_aipw = compute_aipw_att(self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat)
-        self.assertIsInstance(att_aipw, float)
-        # Check if the estimate is still close to the true ATT
-        self.assertAlmostEqual(att_aipw, self.true_att, delta=0.1)
-
+        self.assertNotAlmostEqual(att_aipw, self.true_att, delta=0.1)
 
 # Run the unittests
 if __name__ == "__main__":
--- tests/test_functional/test_tmle.py
+++ tests/test_functional/test_tmle.py
@@ -6,10 +6,11 @@ from CausalEstimate.estimators.functional.tmle import (
     estimate_fluctuation_parameter,
     update_ate_estimate,
 )
-from tests.test_functional.base import TestEffectBase
+from tests.helpers.setup import TestEffectBase
 
 
 class TestTMLEFunctions(TestEffectBase):
+    """Basic tests for TMLE functions"""
     def test_estimate_fluctuation_parameter(self):
         epsilon = estimate_fluctuation_parameter(self.A, self.Y, self.ps, self.Yhat)
         self.assertIsInstance(epsilon, float)
@@ -20,49 +21,22 @@ class TestTMLEFunctions(TestEffectBase):
         epsilon = 0.1  # Arbitrary small fluctuation parameter
         ate = update_ate_estimate(self.ps, self.Y0_hat, self.Y1_hat, epsilon)
         self.assertIsInstance(ate, float)
-        # Check that ate is within a reasonable range
         self.assertTrue(-5 <= ate <= 5)
 
+class TestTMLE_ATE_base(TestEffectBase):
     def test_compute_tmle_ate(self):
         ate_tmle = compute_tmle_ate(
             self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat, self.Yhat
         )
-        self.assertIsInstance(ate_tmle, float)
-        # The true ATE is 2; check if the estimate is close
         self.assertAlmostEqual(ate_tmle, self.true_ate, delta=0.1)
 
-    def test_compute_tmle_ate_edge_cases(self):
-        # Test with ps very close to 0 or 1
-        ps_edge = self.ps.copy()
-        ps_edge[ps_edge < 0.01] = 0.01
-        ps_edge[ps_edge > 0.99] = 0.99
-        ate_tmle = compute_tmle_ate(
-            self.A, self.Y, ps_edge, self.Y0_hat, self.Y1_hat, self.Yhat
-        )
-        self.assertIsInstance(ate_tmle, float)
-        self.assertAlmostEqual(ate_tmle, self.true_ate, delta=0.15)
-
-class TestTMLE_PS_misspecified(TestEffectBase):
+class TestTMLE_PS_misspecified(TestTMLE_ATE_base):
     alpha = [0.1, 0.2, -0.3, 3]
-    def test_compute_tmle_ate(self):
-        ate_tmle = compute_tmle_ate(
-            self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat, self.Yhat
-        )
-        self.assertIsInstance(ate_tmle, float)
-        # The true ATE is 2; check if the estimate is close
-        self.assertAlmostEqual(ate_tmle, self.true_ate, delta=0.1)
-
-class TestTMLE_OutcomeModel_misspecified(TestEffectBase):
+    
+class TestTMLE_OutcomeModel_misspecified(TestTMLE_ATE_base):
     beta = [0.5, 0.8, -0.6, 0.3, 1]
-    def test_compute_tmle_ate(self):
-        ate_tmle = compute_tmle_ate(
-            self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat, self.Yhat
-        )
-        self.assertIsInstance(ate_tmle, float)
-        # The true ATE is 2; check if the estimate is close
-        self.assertAlmostEqual(ate_tmle, self.true_ate, delta=0.1)
 
-class TestTMLE_PS_misspecified_and_OutcomeModel_misspecified(TestEffectBase):
+class TestTMLE_PS_misspecified_and_OutcomeModel_misspecified(TestTMLE_ATE_base):
     alpha = [0.1, 0.2, -0.3, 5]
     beta = [0.5, 0.8, -0.6, 0.3, 5]
     # extreme misspecification
@@ -70,9 +44,6 @@ class TestTMLE_PS_misspecified_and_OutcomeModel_misspecified(TestEffectBase):
         ate_tmle = compute_tmle_ate(
             self.A, self.Y, self.ps, self.Y0_hat, self.Y1_hat, self.Yhat
         )
-        self.assertIsInstance(ate_tmle, float)
-        # The true ATE is 2; check if the estimate is close
-        # will be different from the true ATE
         self.assertNotAlmostEqual(ate_tmle, self.true_ate, delta=0.1)
 
 
