e5b6b04a7af3cb9314eeda73a1aa9ad5aed7bd16
Kiril Klein <kikl@di.ku.dk>
2025-03-12 13:58:06 +0100

    Add RR to TMLE

--- CausalEstimate/estimators/functional/tmle.py
+++ CausalEstimate/estimators/functional/tmle.py
@@ -5,14 +5,118 @@ from statsmodels.genmod.generalized_linear_model import GLM
 import numpy as np
 
 
-def compute_tmle_ate(A, Y, ps, Y0_hat, Y1_hat, Yhat):
+def compute_estimates(A, Y, ps, Y0_hat, Y1_hat, Yhat):
     """
-    Estimate the average treatment effect using the targeted maximum likelihood estimation (TMLE) method.
-    A: treatment assignment, Y: outcome, ps: propensity score,
-    Y0_hat: P[Y|A=0], Y1_hat: P[Y|A=1], Yhat: P[Y]
+    Compute updated outcome estimates using TMLE targeting step.
+
+    Parameters:
+    -----------
+    A: array-like
+        Treatment assignment (0 or 1)
+    Y: array-like
+        Binary outcome
+    ps: array-like
+        Propensity score P(A=1|X)
+    Y0_hat: array-like
+        Initial outcome prediction for control group P(Y|A=0,X)
+    Y1_hat: array-like
+        Initial outcome prediction for treatment group P(Y|A=1,X)
+    Yhat: array-like
+        Combined outcome prediction (A*Y1_hat + (1-A)*Y0_hat)
+
+    Returns:
+    --------
+    tuple: (Q_star_1, Q_star_0)
+        Updated outcome predictions for treatment and control groups
     """
     epsilon = estimate_fluctuation_parameter(A, Y, ps, Yhat)
-    return update_ate_estimate(ps, Y0_hat, Y1_hat, epsilon)
+    Q_star_1, Q_star_0 = update_estimates(ps, Y0_hat, Y1_hat, epsilon)
+    return Q_star_1, Q_star_0
+
+
+def compute_tmle_ate(A, Y, ps, Y0_hat, Y1_hat, Yhat):
+    """
+    Estimate the risk ratio using the targeted maximum likelihood estimation (TMLE) method.
+
+    Parameters:
+    -----------
+    A: array-like
+        Treatment assignment (0 or 1)
+    Y: array-like
+        Binary outcome
+    ps: array-like
+        Propensity score P(A=1|X)
+    Y0_hat: array-like
+        Initial outcome prediction for control group P(Y|A=0,X)
+    Y1_hat: array-like
+        Initial outcome prediction for treatment group P(Y|A=1,X)
+    Yhat: array-like
+        Combined outcome prediction (A*Y1_hat + (1-A)*Y0_hat)
+
+    Returns:
+    --------
+    float: Risk ratio estimate
+    """
+    Q_star_1, Q_star_0 = compute_estimates(A, Y, ps, Y0_hat, Y1_hat, Yhat)
+    return (Q_star_1 - Q_star_0).mean()
+
+
+def compute_tmle_rr(A, Y, ps, Y0_hat, Y1_hat, Yhat):
+    """
+    Estimate the risk ratio using the targeted maximum likelihood estimation (TMLE) method.
+
+    Parameters:
+    -----------
+    A: array-like
+        Treatment assignment (0 or 1)
+    Y: array-like
+        Binary outcome
+    ps: array-like
+        Propensity score P(A=1|X)
+    Y0_hat: array-like
+        Initial outcome prediction for control group P(Y|A=0,X)
+    Y1_hat: array-like
+        Initial outcome prediction for treatment group P(Y|A=1,X)
+    Yhat: array-like
+        Combined outcome prediction (A*Y1_hat + (1-A)*Y0_hat)
+
+    Returns:
+    --------
+    float: Risk ratio estimate
+    """
+    Q_star_1, Q_star_0 = compute_estimates(A, Y, ps, Y0_hat, Y1_hat, Yhat)
+    return Q_star_1.mean() / Q_star_0.mean()
+
+
+def update_estimates(ps, Y0_hat, Y1_hat, epsilon):
+    """
+    Update the initial outcome estimates using the fluctuation parameter.
+
+    Parameters:
+    -----------
+    ps: array-like
+        Propensity score
+    Y0_hat: array-like
+        Initial outcome prediction for control group
+    Y1_hat: array-like
+        Initial outcome prediction for treatment group
+    epsilon: float
+        Estimated fluctuation parameter
+
+    Returns:
+    --------
+    tuple: (Q_star_1, Q_star_0)
+        Updated outcome predictions for treatment and control groups
+    """
+    # Define clever covariates
+    H1 = 1.0 / ps
+    H0 = -1.0 / (1.0 - ps)
+
+    # Update initial estimates with targeting step
+    Q_star_1 = expit(logit(Y1_hat) + epsilon * H1)
+    Q_star_0 = expit(logit(Y0_hat) + epsilon * H0)
+
+    return Q_star_1, Q_star_0
 
 
 def update_ate_estimate(ps, Y0_hat, Y1_hat, epsilon) -> tuple:
